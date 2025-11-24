af1d3192bf7f69ed96765f1ef4dedad48ab2d2d0
Justin <justincrypto702@gmail.com>
2025-11-24 12:02:46 -0800

    Add 16x16 squares all over viewport
    container div now has 100vh and 100vw
    boxes were calculated with window.innerWidth and
    window.innerHeight to determine the amount of
    squares into column * rows for amount
    
    loop created an element div, attributed it and
    appended this element to parent flexbox

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+document.body.setAttribute('style', `margin: 0; padding: 0;`);
 const containerDiv = document.createElement("div");
 containerDiv.setAttribute('class', 'container');
-containerDiv.style.display = "flex";
-
+containerDiv.setAttribute('style', `height: 100vh; width: 100vw; display: flex; flex-wrap: wrap; ` );
 document.body.appendChild(containerDiv);
 containerDiv.style.border = "2px solid";
 
-const squareBox = document.createElement("div");
-squareBox.style.border = "2px solid";
-squareBox.style.height = "16px";
-squareBox.style.width = "16px";
-containerDiv.append(squareBox);
+
+function maxSquare(){
+ const screenWidth = window.innerWidth;
+ const screenHeight = window.innerHeight;
+ const boxSize = 16;
+ const columns = Math.floor(screenWidth/boxSize);
+ const rows = Math.floor(screenHeight/boxSize);
+ const totalSquares = columns * rows;
+
+ for(let i=0; i<totalSquares;i++){
+   const newBox = document.createElement("div");
+   newBox.setAttribute('style', `border: 1px solid #ccc; height: 16px; width: 16px;`);
+   containerDiv.append(newBox);
+ }
+    
+}
+maxSquare();
